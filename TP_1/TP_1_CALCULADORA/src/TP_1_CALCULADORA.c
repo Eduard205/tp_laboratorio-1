cd1397d7088f697b4f81585b7a210a72e97be340
Eduard205 <eduardbrito205@gmail.com>
2021-04-16 18:34:35 -0300

    Proyecto modificado 2

--- TP_1/TP_1_CALCULADORA/src/TP_1_CALCULADORA.c
+++ TP_1/TP_1_CALCULADORA/src/TP_1_CALCULADORA.c
@@ -1,10 +1,34 @@
 /*
  ============================================================================
  Name        : TP_1_CALCULADORA.c
- Author      : 
- Version     :
- Copyright   : Your copyright notice
- Description : Hello World in C, Ansi-style
+ Author      : Eduard Amilcar Brito Hernandez - División 1-E
+
+Hacer una calculadora. Para ello el programa iniciará y contará con un menú de opciones:
+
+1.  Ingresar 1er operando (A=x)
+
+2.  Ingresar 2do operando (B=y)
+
+3.  Calcular todas las operaciones
+a)  Calcular la suma (A+B)
+b)  Calcular la resta (A-B)
+c)  Calcular la division (A/B)
+d)  Calcular la multiplicacion (A*B)
+e)  Calcular el factorial (A!)
+
+4.  Informar resultados
+a)  “El resultado de A+B es: r”
+b)  “El resultado de A-B es: r”
+c)  “El resultado de A/B es: r” o “No es posible dividir por cero”
+d)  “El resultado de A*B es: r”
+e)  “El factorial de A es: r1 y El factorial de B es: r2”
+
+5.  Salir
+
+• Todas las funciones matemáticas del menú se deberán realizar en una biblioteca aparte, que contenga las funciones para realizar las cinco operaciones.
+• En el menú deberán aparecer los valores actuales cargados en los operandos A y B (donde dice “x” e “y” en el ejemplo, se debe mostrar el número cargado).
+• Deberán contemplarse los casos de error (división por cero, etc).
+• Documentar todas las funciones.
  ============================================================================
  */
 
@@ -12,6 +36,9 @@
 #include <stdlib.h>
 
 int main(void) {
+setbuf(stdout, NULL);
+
+
 	printf("Proyecto Modificado");
 	return EXIT_SUCCESS;
 }
