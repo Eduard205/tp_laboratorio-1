bca1d1ced6ad8837677dd5dcab55ae3b29ef8b5b
Eduard Brito <eduardbrito205@gmail.com>
2021-11-20 22:42:41 -0300

    LikedList intregrado OK

---
+++ TP_4/TP4 Integrado con linkedlist/src/utn.h
@@ -0,0 +1,25 @@
+/*
+ * utn.h
+ *Biblioteca de validaciones UTN
+ *
+ *      Author: Eduard Brito
+ */
+
+#ifndef UTN_H_
+#define UTN_H_
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+
+char pedirCadena(char Char[],int tamano, int reintento, char* pTexto, char* pTextoError);
+int pedirEntero(int* pInt, int min, int max, int reintento, char* pTexto, char* pTextoError);
+float pedirFlotante(float* pFloat /*int min, int max*/, int reintento, char* pTexto, char* pTextoError);
+int utn_pedirCuit(char auxiliar[], char *mensaje, char *mensajeError, int reintentos);
+int utn_getCharRespSoN(char *variableChar, char *mensaje, char *mensajeError, int reintentos);
+void mostrarMenu(int *opcion);
+void mostrarSubMenu(int *opcion);
+int esNumeroValidacion(char cadena[]);
+void utn_convertirIntACadena(int cadenaDeInt, char* intCovertidoAString);
+
+#endif /* UTN_H_ */
